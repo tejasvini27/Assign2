09891a58132ae00eb55f1c0b42acc1fe1f4a4d1d
Tejasvini Neelarapu <tejasvini.neelarapu@mergencorp.com>
2025-12-26 16:09:50 +0530

    first commit

---
+++ Javascript/script.js
@@ -0,0 +1,25 @@
+ /*let a = 5;
+let b = 3;
+console.log("a + b = ", a + b);*/
+
+//Conditional Statements
+/* let age = 16;
+if (age >= 18) {
+    console.log("you can vote");
+}
+if (age < 18) {
+    console.log("You cannot vote");
+} */
+
+    let mode = dark;
+    let color;
+    if(mode===dark){
+        color="black";
+    } else if (mode===blue) {
+        color="blue";
+    } else if (mode===pink) {
+        color="pink";
+    } else{
+        color="white"
+    }
+    console.log(color);
